5a5f303fa5f9fee3155519d25970f150a5b6e66c
bogomolovvm <bogomolovvladislav85@gmail.com>
2026-06-21 21:53:50 +0700

    fix: wrong field name in remane request

--- src/main/java/org/example/diplom/dto/RenameRequest.java
+++ src/main/java/org/example/diplom/dto/RenameRequest.java
@@ -1,8 +1,10 @@
 package org.example.diplom.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 @Data
 public class RenameRequest {
+    @JsonAlias("filename")
     private String name;
 }
